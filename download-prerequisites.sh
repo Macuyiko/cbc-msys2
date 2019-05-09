65fd02449c029d838a00337148e454a60d95bb61
Seppe vanden Broucke <macuyiko@gmail.com>
2019-05-09 13:17:50 +0200

    Update download-prerequisites.sh

--- download-prerequisites.sh
+++ download-prerequisites.sh
@@ -8,15 +8,15 @@ rm -R $DOWNLOADDIR
 mkdir $DOWNLOADDIR
 
 echo 
-echo 'Downloading prerequisites (CBC, jCbc, swigwin, OpenBLAS)'
+echo 'Downloading prerequisites (CBC, trunk-cbc, jCbc, swigwin, OpenBLAS)'
 echo 'Modify this script in case you want to change versions'
 echo 'Downloading trunk-cbc uses svnkit, modify the JAVA_HOME if needed'
 echo 
 
-wget https://www.coin-or.org/download/source/Cbc/Cbc-2.10.0.zip -O $DOWNLOADDIR/cbc.zip
+wget https://www.coin-or.org/download/source/Cbc/Cbc-2.10.2.zip -O $DOWNLOADDIR/cbc.zip
 wget https://github.com/JNICbc/jCbc/archive/master.zip -O $DOWNLOADDIR/jcbc.zip
 wget http://prdownloads.sourceforge.net/swig/swigwin-3.0.12.zip -O $DOWNLOADDIR/swigwin.zip
-wget https://github.com/xianyi/OpenBLAS/archive/v0.3.5.zip -O $DOWNLOADDIR/openblas.zip
+wget https://github.com/xianyi/OpenBLAS/archive/v0.3.6.zip -O $DOWNLOADDIR/openblas.zip
 
 # svn doesn't work quite right on MSYS2, so use svnkit instead
 wget https://www.svnkit.com/org.tmatesoft.svn_1.9.3.standalone.zip -O $DOWNLOADDIR/svnkit.zip
